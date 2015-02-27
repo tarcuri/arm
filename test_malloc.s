c1fec64cc6e56791d2df13731c94a630f616d927
Thomas J. Arcuri <thomas.j.arcuri@gmail.com>
2015-02-27 03:20:46 +0000

    now correctly prints malloc'd address

--- test_malloc.s
+++ test_malloc.s
@@ -1,6 +1,8 @@
 .data
 alloc_msg:
-  .string "Allocated %d bytes @ 0x%x\n> "
+  .string "Allocated %d bytes @ 0x%x\n"
+input_msg:
+  .string "Enter up to 1024 characters for printing\n"
 value_msg:
   .string "Valued stored: %d\n"
 
@@ -18,16 +20,17 @@ main:
   bl    malloc
   ldr   %r1, =mem_ptr
   str   %r0, [%r1]
+  ldr   %r2, [%r1]
   ldr   %r0, =alloc_msg
   mov   %r1, $1024
-  ldr   %r2, =mem_ptr
   bl    printf
-  ldr   %r2, =mem_ptr
-  mov   %r0, $16
-  str   %r0, [%r2]
-  ldr   %r0, =value_msg
-  ldr   %r3, =mem_ptr
-  ldr   %r1, [%r3]
+  ldr   %r0, =input_msg
+  bl    printf
+  mov   %r0, $0
+  ldr   %r1, =mem_ptr
+  mov   %r2, $1024
+  bl    read
+  ldr   %r0, =mem_ptr
   bl    printf
 
 exit:
